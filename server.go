be789c8a49b71bcaf461184aaac53847cd99412a
david <david@RDavid.localdomain>
2025-10-13 16:13:01 +0200

    Grilles

--- server.go
+++ server.go
@@ -6,9 +6,41 @@ import (
 	"net/http"
 )
 
+var GrilleF = [6][7]int{}
+var GrilleM = [5][6]int{}
+var GrilleD = [4][5]int{}
+
 func main() {
+	// Initialisation des grilles
+	GrilleF = [6][7]int{
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+	}
+
+	GrilleM = [5][6]int{
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+	}
+
+	GrilleD = [4][5]int{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+	}
+
+	// Routes
 	http.HandleFunc("/", HomeHandler)
-	http.HandleFunc("/play", PlayHandler)
+	http.HandleFunc("/play", PlayFHandler)
+	http.HandleFunc("/play", PlayMHandler)
+	http.HandleFunc("/play", PlayDHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -20,11 +52,26 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-func PlayHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("pages/play.html", "pages/templates/header.html", "pages/templates/footer.html")
+func PlayFHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("pages/playF.html", "pages/templates/header.html", "pages/templates/footer.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 	tmpl.Execute(w, nil)
 }
 
+func PlayMHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("pages/playM.html", "pages/templates/header.html", "pages/templates/footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, nil)
+}
+
+func PlayDHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("pages/playD.html", "pages/templates/header.html", "pages/templates/footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, nil)
+}
